1e6517e495323cf764a43cf3e42afcbf13d117a0
Rey Villa <reyv10@gmail.com>
2012-10-20 16:27:31 -0400

    Added model to include email from contact for to db.

--- models.py
+++ models.py
@@ -39,3 +39,9 @@ class Admin(db.Model):
         user_id = user_id_cookie_val.split('|')[0]
         user_key = db.Key.from_path('Admin', int(user_id))
         return db.get(user_key)
+
+
+class SubscribeEmail(db.Model):
+    """Model class for Receiving Subscribe Emails"""
+    email = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
