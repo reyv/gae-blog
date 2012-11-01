50981c28735d0972981720e0d9fd029df593fa49
Rey Villa <reyv10@gmail.com>
2012-11-01 11:52:11 -0400

    Renamed to start with blog_. This will make it easier to merge blog with other projects.

---
+++ blog_config.py
@@ -0,0 +1,26 @@
+#Blog Name
+blog_name = 'My Blog'
+
+#Author name
+blog_author = 'Author A'
+
+#Default Admin Credentials. Password should be
+#changed upon initialization of blog
+admin_username = 'admin'
+admin_pw = 'password'
+
+#Admin salt
+salt_length = 5
+
+#Cookie
+cookie_secret = 'this is my secret key'
+
+#Social Media
+google_plus_url = 'https://plus.google.com/URL'
+twitter_url = 'http://www.twitter.com/URL'
+linkedin_url = 'http://www.linkedin.com/company/URL'
+
+#Contact for e-mails
+
+email_to = 'test@test.com'
+email_from = 'test@test.com'
---
+++ blog_models.py
@@ -0,0 +1,49 @@
+import blog_config
+from google.appengine.ext import db
+
+
+class BlogPost(db.Model):
+    """Model class for blog posts"""
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    post_id = db.StringProperty()
+    image_url = db.StringProperty()
+    tag = db.StringProperty(required=True)
+    author = db.StringProperty()
+    visits = db.IntegerProperty(default=0)
+
+
+class PostPreview(BlogPost):
+    """Model class for blog post previews"""
+    pass
+
+
+class Admin(db.Model):
+    """Model class for Admin login"""
+    admin_username = db.StringProperty(required=True,
+                                        default=blog_config.admin_username)
+    admin_pw_hash = db.StringProperty(required=True,
+                                        default=blog_config.admin_pw)
+
+    @classmethod
+    def login_validation(cls, username):
+        try:
+            q = db.GqlQuery("SELECT * from Admin WHERE admin_username='%s'"
+                                % username)
+            return q[0]
+        except IndexError:
+            return None
+
+    @classmethod
+    def get_user(cls, user_id_cookie_val):
+        user_id = user_id_cookie_val.split('|')[0]
+        user_key = db.Key.from_path('Admin', int(user_id))
+        return db.get(user_key)
+
+
+class SubscribeEmail(db.Model):
+    """Model class for Receiving Subscribe Emails"""
+    email = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
